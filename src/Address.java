3d5bbfaf14829a2ae674207297fe48a098ce12f7
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-12-22 21:45:28 +0900

    feat: 전체 주소록을 출력하는 기능 추가

--- src/Address.java
+++ src/Address.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 public class Address {
     private Map<String, String> addressMap;
 
-    public Address() {
     public Address() {  // 생성자 : Address 객체를 생성할 때 내부 Map을 초기화
         addressMap = new HashMap<>();
     }
@@ -17,7 +16,13 @@ public class Address {
         return addressMap.get(name);
     }
 
-    public void removeAddress(String name) {
+    public void removeAddress(String name) {    // 주소 삭제
         addressMap.remove(name);
+    }
+
+    public void printAddress() {    // 전체 주소록 출력
+        for(Map.Entry<String, String> entry : addressMap.entrySet()) {
+            System.out.println("Name : " + entry.getKey() + ", Address : " + entry.getValue());
+        }
     }
 }
