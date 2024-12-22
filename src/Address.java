9dada5d6563b670edd222a3408a4b75facda5814
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-12-22 21:39:23 +0900

    feat: Address 객체를 관리하는 기능 추가

--- src/Address.java
+++ src/Address.java
@@ -5,6 +5,19 @@ public class Address {
     private Map<String, String> addressMap;
 
     public Address() {
+    public Address() {  // 생성자 : Address 객체를 생성할 때 내부 Map을 초기화
         addressMap = new HashMap<>();
+    }
+
+    public void addAddress(String name, String address) {   // 주소 추가
+        addressMap.put(name, address);
+    }
+
+    public String getAddress(String name) { // 주소 조회
+        return addressMap.get(name);
+    }
+
+    public void removeAddress(String name) {
+        addressMap.remove(name);
     }
 }
