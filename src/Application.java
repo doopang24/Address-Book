fd4bbaf20c295448abc07a998d7d0d38b1406f4a
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-01-03 13:54:55 +0900

    feat: 시작 메세지 출력 메서드 작성

--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,12 @@
 public class Application {
+    public static void main(String[] args) {
+        Application application = new Application();
+
+        application.printStartMessage();
+    }
+
+    private void printStartMessage() {
+        System.out.println("주소록 프로그램에 오신 것을 환영합니다.");
+        System.out.println("====================================");
+    }
 }
