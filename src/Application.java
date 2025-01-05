82d0553fd9512897c1ca9ad021ecafa937458829
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-01-05 22:51:56 +0900

    feat: 메뉴를 보여주고 명령을 입력받는 기능 추가

--- src/Application.java
+++ src/Application.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class Application {
+
+    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         Application application = new Application();
 
         application.printStartMessage();
+        application.showMenu();
+        int order = application.receiveOrder();
     }
 
     private void printStartMessage() {
         System.out.println("주소록 프로그램에 오신 것을 환영합니다.");
         System.out.println("====================================");
+    }
+
+    private void showMenu() {
+        System.out.println("원하는 기능의 숫자를 입력하세요.");
+        System.out.println("1 : 주소 추가");
+        System.out.println("2 : 주소 찾기");
+        System.out.println("3 : 주소 삭제");
+        System.out.println("4 : 주소록 초기화");
+        System.out.println("5 : 프로그램 종료");
+    }
+
+    private int receiveOrder() {
+        return scanner.nextInt();
     }
 }
